44eba14dd6c0da4d6b5225481d99be3decf8f96d
Varsha Sakaria <sakariavarsha2000@gmail.com>
2021-12-08 16:58:49 +0530

    added login endpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+
+import { bubble } from "svelte/internal";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/login">
+<button>Login using Github</button>
+</a>
